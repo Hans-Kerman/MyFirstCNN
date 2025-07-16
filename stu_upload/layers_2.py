41d4e9f3786e187b728e30087a80b4d3776a4563
ButterCat-surface <62628392+Hans-Kerman@users.noreply.github.com>
2025-07-16 12:59:08 +0800

    Complete the ConvolutionalLayer

--- stu_upload/layers_2.py
+++ stu_upload/layers_2.py
@@ -15,31 +15,47 @@ def show_time(time, name):
 class ConvolutionalLayer(object):
     def __init__(self, kernel_size, channel_in, channel_out, padding, stride):
         # 卷积层的初始化
-        self.kernel_size = kernel_size
+        self.kernel_size = kernel_size  #卷积核大小k
         self.channel_in = channel_in
         self.channel_out = channel_out
-        self.padding = padding
-        self.stride = stride
+        self.padding = padding  #边缘填充(的尺寸)
+        self.stride = stride    #滑动步长
         print('\tConvolutional layer with kernel size %d, input channel %d, output channel %d.' % (self.kernel_size, self.channel_in, self.channel_out))
-    def init_param(self, std=0.01):  # 参数初始化
+    def init_param(self, std=0.01):  # 参数初始化(生成卷积核)
         self.weight = np.random.normal(loc=0.0, scale=std, size=(self.channel_in, self.kernel_size, self.kernel_size, self.channel_out))
         self.bias = np.zeros([self.channel_out])
     def forward(self, input):  # 前向传播的计算
         start_time = time.time()
         self.input = input # [N, C, H, W]
+        """
+        N:Batch_size样本数
+        C:Channels通道数
+        H:特征图高度
+        W:特征图宽度
+        """
         height = self.input.shape[2] + self.padding * 2
         width = self.input.shape[3] + self.padding * 2
         self.input_pad = np.zeros([self.input.shape[0], self.input.shape[1], height, width])
         self.input_pad[:, :, self.padding:self.padding+self.input.shape[2], self.padding:self.padding+self.input.shape[3]] = self.input
-        height_out = (height - self.kernel_size) / self.stride + 1
-        width_out = (width - self.kernel_size) / self.stride + 1
+        height_out = (height - self.kernel_size) // self.stride + 1
+        width_out = (width - self.kernel_size) // self.stride + 1
         self.output = np.zeros([self.input.shape[0], self.channel_out, height_out, width_out])
-        for idxn in range(self.input.shape[0]):
-            for idxc in range(self.channel_out):
-                for idxh in range(height_out):
-                    for idxw in range(width_out):
+        for idxn in range(self.input.shape[0]):         #idx n：第idxn张图片
+            for idxc in range(self.channel_out):        #idx c：第idxc个通道
+                for idxh in range(height_out):          #idx h：第idxh行像素
+                    for idxw in range(width_out):       #idx w：第w列的像素
                         # TODO: 计算卷积层的前向传播，特征图与卷积核的内积再加偏置
-                        self.output[idxn, idxc, idxh, idxw] = _______________________
+
+                        # 1. 提取 input_patch (和之前一样)
+                        start_h = idxh * self.stride
+                        start_w = idxw * self.stride
+                        input_patch = self.input_pad[idxn, :, start_h:start_h + self.kernel_size, start_w:start_w + self.kernel_size]
+
+                        # 2. 提取 current_kernel (根据你的权重维度顺序)
+                        # 假设你的权重维度是 (C_in, K, K, C_out)
+                        current_kernel = self.weight[:, :, :, idxc]
+
+                        self.output[idxn, idxc, idxh, idxw] = input_patch.flatten() @ current_kernel.flatten() + self.bias[idxc]
         return self.output
     def load_param(self, weight, bias):  # 参数加载
         assert self.weight.shape == weight.shape
